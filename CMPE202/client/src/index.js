6cfeee29dc590f2b942c9dca2b69ca93ae18bc7a
smduvvuri <shreemaduvvuri@gmail.com>
2022-05-07 09:47:17 -0700

    testing

--- CMPE202/client/src/index.js
+++ CMPE202/client/src/index.js
@@ -4,18 +4,16 @@ import "./index.css";
 import "antd/dist/antd.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-// 1. import from react-redux and redux
+
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "./reducers";
 
-// 2. create user reducer function
-// 3. combine multiple reducers
-// 4. create redux store
+
 const store = createStore(rootReducer, composeWithDevTools());
 
-// 5.  provice redux store to the entire app
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -26,7 +24,4 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
